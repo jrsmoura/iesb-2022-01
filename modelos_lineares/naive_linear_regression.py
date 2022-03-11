aca3887aa323cf4fa5f384ea4b8837bd5e98363c
José Roberto Steiner de Moura <jrsteiner78@gmail.com>
2022-03-11 17:09:34 -0300

    ./modelos_lineares/naive_linear_regression.py implementation. Still need two functions to be implemented.

--- modelos_lineares/naive_linear_regression.py
+++ modelos_lineares/naive_linear_regression.py
@@ -6,10 +6,10 @@ class NaiveLinearRegression:
     """
     Class for a naive implementation of linear regression
     """
-    def __init__(self, n_points: int, parameter: float):
+    def __init__(self, n_points: int, coefficient: float) -> None:
         self.error_msg = "to be implemented"
         self.n_points = n_points
-        self.parameter = parameter
+        self.coefficient = coefficient
 
     @staticmethod
     def hat_matrix(self):
@@ -17,17 +17,32 @@ class NaiveLinearRegression:
 
     @staticmethod
     def parameters_calc(self):
-        return AssertionError(self.error_msg)
+        """ Function responsible for calculate the model parameter
+        :arg:
+            - _x (list) input data
+            - hat_matrix (list)
 
-    def synthetic_data(self):
+        :returns:
+            - (float) fitting parameter"""
+        return np.dot(self._x, self.hat_matrix)
+
+    def synthetic_data(self) -> list:
+        """ Function responsible for create the synthetic.
+        :arg:
+            - n_points (int) number of points in each axis
+            - coefficient (float) coefficient used to construct _y from _x
+
+        :returns:
+            - x, y (list) two lists containing the synthetic data
+        """
         _x = np.random.rand(self.n_points)
         _noise = np.random.rand(self.n_points)
-        _y = self.parameter * _x + _noise
+        _y = self.coefficient * _x + _noise
 
         return _x, _y
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     x, y = NaiveLinearRegression(50, 1.2).synthetic_data()
 
     plt.scatter(x, y)
