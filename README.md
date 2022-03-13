67f4a07ae20fcab8e3b3b1c591e180f20b4872ed
José Roberto Steiner de Moura <jrsteiner78@gmail.com>
2022-03-13 20:15:32 -0300

    Some updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# iesb-2022-01
-Códigos das disciplinas do curso de DS e IA do IESB. Primeiro semestre de 2022
+# Primeiro Semestre de 2022 - IESB
+
+Códigos das disciplinas do curso de DS e IA do IESB.
+
+## Disciplinas
+
+- Deep Learning
+- Modelos Lineares
+- Probabilidades
+
+**Obs**.: O arquivo `requirements.txt` é o mesmo para todas as disciplinas, mas os que não forem da matéria
+de Deep Learning, observem que o arquivo contem o pacote `tensorflow`. Caso não o queiram, apenas
+removam a linha que o contem.
---
+++ deep_learning/basics/autodiff.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class AutoDiff(object):
+    def __init__(self, epsilon: list) -> None:
+        self.error_message = "To Be Implemented"
+        self.epsilon = epsilon
+
+    def func(self):
+        return AssertionError(self.error_message)
+
+    def finite_diference(self):
+        return AssertionError(self.error_message)
+
+    def foward_mode_autodiff(self):
+        return AssertionError(self.error_message)
+
+    def reverse_autodiff(self):
+        return AssertionError(self.error_message)
+
+
+if __name__ == '__main__':
+    pass
